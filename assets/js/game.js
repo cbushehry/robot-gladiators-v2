ac9a4e81231b719425a80225b86df47ef7efc91c
Cameron Bushehry <87917675+cbushehry@users.noreply.github.com>
2022-03-20 20:16:23 -0700

    fightOrSkip bug fixes v1

--- assets/js/game.js
+++ assets/js/game.js
@@ -19,9 +19,11 @@ while (playerInfo.health > 0 && enemy.health > 0) {
     // if yes (true), leave fight
     if (confirmSkip) {
       window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-      // subtract money from playerMoney for skipping
-      playerInfo.playerMoney = playerInfo.money - 10;
-      shop();
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      // return true if player wants to leave
+      return true;
     }
   }
 }
